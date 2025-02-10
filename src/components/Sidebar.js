9a73242f1798235a50756d23cd57510873f37c04
Yaman Maharjan <yamanmaharjan00@gmail.com>
2025-02-10 17:49:01 +0545

    first commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Sidebar = () => {
+    return <div>
+        <div className="sidebar">
+            <h3>Yaman Maharjan</h3></div></div>;
+};
+
+export default Sidebar;
+
+
+
