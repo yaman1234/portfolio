77490661c188ccf21ae2b4376d59e48e5a0422b0
Yaman Maharjan <yamanmaharjan00@gmail.com>
2025-02-18 16:17:24 +0545

    added download CV feature

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,32 @@
 import { VerticalLeftOutlined } from "@ant-design/icons";
 import React from "react";
+import cv from "../assets/ProfessionalResume.pdf";
 
 const Header = () => {
-    return <div>
-        <div className="header">
-            <div className="logo">
-                <span><VerticalLeftOutlined /></span>
-            </div>
-            <ul>
-                <li>About</li>
-                <li>Work</li>
-                <li>Contact</li>
-            </ul>
-            <div className="right">
-                <button>Dark Mode</button>
-                <button>Download CV</button>
-            </div>
+  return (
+    <div>
+      <div className="header">
+        <div className="logo">
+          <span>
+            <VerticalLeftOutlined />
+          </span>
         </div>
-    </div>;
+        <ul>
+          <li>About</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+        <div className="right">
+          <button>Dark Mode</button>
+          <button>
+            <a href={cv} download="Resume.pdf">
+              Download CV
+            </a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
